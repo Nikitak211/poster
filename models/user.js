2e80386b7a882d86b7a5a0a6b1de4c6e431bd2ed
nikita <git@github.com:Nikitak211/temp-node.js.git>
2021-10-04 21:42:14 +0300

    database working,progress was made

--- models/user.js
+++ models/user.js
@@ -4,12 +4,6 @@ const Schema = mongoose.Schema;
 //Schema creater and models
 
 const userSchema = new Schema({
-    firstName: {
-        type: String
-    },
-    lastName: {
-        type: String
-    },
     username: {
         type: String,
         required: true},
